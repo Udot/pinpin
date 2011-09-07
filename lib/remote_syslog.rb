83a8d6fb64d4ba2f39055a49dbae7f56af60be7e
mcansky <riboulet@gmail.com>
2011-09-07 16:03:39 +0200

    massive refactoring using code from Harry

---
+++ lib/remote_syslog.rb
@@ -0,0 +1,20 @@
+require "remote_syslog_logger"
+class RemoteSyslog
+  def initialize(host, port)
+    @logger = RemoteSyslogLogger.new(host, port, {:program => 'pinpin'})
+  end
+
+  def info(msg)
+    @logger.info(msg)
+  end
+  def error(msg)
+    @logger.error(msg)
+  end
+  def warn(msg)
+    @logger.warn(msg)
+  end
+
+  def write(str)
+    @logger.info(str)
+  end
+end
--- pinpin.rb
+++ pinpin.rb
@@ -3,7 +3,6 @@ require "rubygems"
 require "json"
 require "redis"
 require 'fog'
-
 require "digest/sha1"
 require "yaml"
 require "fileutils"
@@ -22,129 +21,182 @@ def environment
 end
 
 @current_path = File.expand_path(File.dirname(__FILE__))
-
+require "#{@current_path}/lib/remote_syslog"
+LOGGER = RemoteSyslog.new(Settings.remote_log_host,Settings.remote_log_port)
 @config = YAML.load_file("#{@current_path}/config.yml")[environment]
+# queue in
 @redis = Redis.new(:host => @config['redis']['host'], :port => @config['redis']['port'], :password => @config['redis']['password'], :db => @config['redis']['database'])
+# queue out
+@redis_cuddy = Redis.new(:host => @config['redis']['host'], :port => @config['redis']['port'], :password => @config['redis']['password'], :db => @config['redis']['cuddy_database'])
+# global status db
+@redis_global = Redis.new(:host => @config['redis']['host'], :port => @config['redis']['port'], :password => @config['redis']['password'], :db => @config['redis']['status_database'])
 
-def logger(severity, message)
-  file = File.open(@config['logfile'], "a")
-  case severity
-  when "info"
-    file.puts "I :: #{Time.now.to_s} : INFO : " + message
-  when "error"
-    file.puts "E :: #{Time.now.to_s} : ERROR : " + message
-  when "fatal"
-    file.puts "F :: #{Time.now.to_s} : FATAL : " + message
-  end
-  file.close
+def logger
+  LOGGER
 end
 
-def build(repository = nil, version = nil, backoffice = false, cuddy_token)
-  if cuddy_token
-    redis_deploy = Redis.new(:host => @config['redis']['host'], :port => @config['redis']['port'], :password => @config['redis']['password'], :db => 1)
+class Build
+  attr_accessor :name, :repository, :version, :cuddy_token, :start_time
+  def initialize(name, repository, db_string, cuddy_token)
+    logger.info("initializing build for #{name}")
+    @name = name
+    @repository = repository
+    @cuddy_token = cuddy_token
+    @current_path = File.expand_path(File.dirname(__FILE__))
+    @db_string = db_string
+    @start_time = start_time_from_redis
+    repositories = Hash.new
+    repositories = YAML.load_file(@current_path + "/config/repositories.yml") if File.exist?(@current_path + "/config/repositories.yml")
+    if repositories[name] == nil
+      repositories[name] = {"name" => name, "repository" => repository, "version" => "1"}
+      FileUtils.mkdir(@current_path + "/config") unless File.exist?(@current_path + "/config")
+      File.open(@current_path + "/config/repositories.yml", 'w' ) do |out|
+        YAML.dump(repositories, out)
+      end
+    end
+    @version = repositories[name]["version"]
   end
-  logger("info", "build for #{repository} #{version} starts.")
-  status = JSON.parse(@redis.get(repository)) if (@redis.get(repository) != nil)
-  start_time = status['started_at'] 
-  if status != nil
-    if (status["status"] == ("building" || "built"))
-      logger("info", "already built or being built elsewhere")
-      return true
+
+  def next_version
+    return version + 1
+  end
+
+  def start_time_from_redis
+    node = @redis_global.get(name)
+    return JSON.parse(node)['started_at'] if node != nil
+    return Time.now
+  end
+
+  def run
+    status = {"status" => "building", "version" => build.version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
+    @redis_global.set(build.name, status)
+    logger.info("cloning build for #{name}")
+    self.version = next_version
+    FileUtils.mkdir("/var/build/#{name}") unless File.exist?("/var/build/#{name}")
+    Dir.chdir("/var/build/#{name}")
+    clone_shallow = `git clone --depth 1 #{repository} #{version}`
+    FileUtils.rm_rf("#{version}/.git")
+    logger.info("bundling #{name} v#{version} in /var/build/#{name}/#{version}")
+    Dir.chdir("/var/build/#{name}/#{version}")
+    log = `bundle install --deployment --without development test`
+    Dir.chdir("/var/build/#{name}")
+    logger.info("packing #{name} v#{version}")
+    log = `tar -czf /var/build/#{name}/#{name}-#{version}.tar.gz #{version}`
+    logger.info("cleaning up #{name} #{version} build folder")
+    FileUtils.rm_rf("/var/build/#{name}/#{version}")
+  end
+
+  def save
+    logger.info("saving #{name} version (v#{version})")
+    repositories = YAML.load_file(@current_path + "/config/repositories.yml") if File.exist?(@current_path + "/config/repositories.yml")
+    repositories[name] = {"name" => name, "repository" => repository, "version" => version}
+    FileUtils.mkdir(@current_path + "/config") unless File.exist?(@current_path + "/config")
+    File.open(@current_path + "/config/repositories.yml", 'w' ) do |out|
+      YAML.dump(repositories, out)
     end
   end
-  status = {"status" => "building", "version" => version, "started_at" => start_time, "finished_at" => "", "error" => {"message" => "", "backtrace" => ""}}.to_json
-  @redis.set(repository, status)
-  begin
-    return true if ((repository == nil) && (version == nil))
+
+  def upload
+    logger.info("uploading #{name} v#{version} in the Cloud")
     current_path = File.expand_path(File.dirname(__FILE__))
-    path = repository.split('/').last.gsub(/.git$/,'')
-    FileUtils.mkdir("#{@config["build"]["root"]}") unless File.exist?("#{@config["build"]["root"]}")
-    FileUtils.rm_rf("#{@config["build"]["root"]}/#{path}/#{version}") if (File.exist?("#{@config["build"]["root"]}/#{path}") && File.exist?("#{@config["build"]["root"]}/#{path}/#{version}"))
-
-    logger("info", "cloning #{repository} #{version}")
-    checkout_cmd = "git clone --depth 1 #{repository} #{version}"
-    FileUtils.mkdir("#{@config["build"]["root"]}/#{path}") unless File.exist?("#{@config["build"]["root"]}/#{path}")
-    Dir.chdir("#{@config["build"]["root"]}/#{path}")
-    checkout_log = `#{checkout_cmd}`
-    raise SystemCallError, checkout_log unless $?.to_i == 0
-
-    img = ""
-    rs_dir = ""
-    img_cmd = ""
-    img_root = "#{@config["build"]["root"]}"
-    logger("info", "bundle install #{repository} #{version}")
-    FileUtils.mkdir_p("#{@config["build"]["root"]}/#{path}/#{version}/vendor/bundle")
-    bundle_log = `cd #{@config["build"]["root"]}/#{path}/#{version} && bundle install --deployment --without development test`
-    raise SystemCallError, bundle_log unless $?.to_i == 0
-    raise ArgumentError, "bundled in the wrong place" unless File.exist?("#{@config["build"]["root"]}/#{path}/#{version}/vendor/bundle")
-    
-    if is_linux?  
-      img = "#{path}-#{version}.tgz"
-      rs_dir = "sqshed_apps"
-    elsif is_mac?
-      img = "#{path}-#{version}.tgz"
-      rs_dir = "sqshed_apps_test"
-    else
-      raise ArgumentError, "platform is not correct"
-    end
-    img_cmd = "tar -czf #{img} #{path}/#{version}"
-    Dir.chdir(img_root)
-    img_log = `#{img_cmd}`
-    raise SystemCallError, img_log unless $?.to_i == 0
-    logger("info", "creating image #{repository} #{version}")
-
-    storage = Fog::Storage.new(:provider => 'Rackspace', :rackspace_auth_url => @config["rackspace_auth_url"], :rackspace_api_key => @config["rackspace_api_key"], :rackspace_username => @config['rackspace_username'])
+  	config = YAML.load_file(current_path + "/config.yml")
+  	rs_dir = "sqshed_apps"
+  	storage = Fog::Storage.new(:provider => 'Rackspace', :rackspace_auth_url => config["rackspace_auth_url"], :rackspace_api_key => config["rackspace_api_key"], :rackspace_username => config['rackspace_username'])
     directory = storage.directories.get(rs_dir)
-    directory.files.create(:key => "#{img}", :body => File.open("#{@config["build"]["root"]}/#{img}"))
-    FileUtils.rm_rf("#{@config["build"]["root"]}/#{path}") if File.exist?("#{@config["build"]["root"]}/#{path}")
-  rescue => e
-    p e.message
-    p e.backtrace
-    logger("error", e.message)
-    status = {"status" => "failed", "version" => version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => e.message, "backtrace" => e.backtrace}}.to_json
-    @redis.set(repository, status)
+    directory.files.create(:key => "#{name}-#{version}.tar.gz", :body => File.open("/var/build/#{name}/#{name}-#{version}.tar.gz"))
+    FileUtils.rm_rf("/var/build/#{name}/#{name}-#{version}.tar.gz") if File.exist?("/var/build/#{name}/#{name}-#{version}.tar.gz")
+    status = {"status" => "uploaded", "version" => build.version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
+    @redis_global.set(build.name, status)
   end
-  logger("info", "built and uploaded #{repository} #{version}")
-  status = {"status" => "built", "version" => version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
-  @redis.set(repository, status)
-  #  {"version" => integer,      # the version number
-  #   "name" => string,           # the name of the app
-  #   "status" => string,         # starts with "waiting"
-  #   "started_at" => datetime,   # the time when the app was added in the queue
-  #   "finished_at" => datetime,  # the time when the app was properly deployed
-  #   "backoffice" => boolean,      # is the app a backoffice thing (will not create db and use different init script)
-  #   "config" => { "unicorn" => { "workers" => integer },      # only if not back office
-  #     "db" => {"hostname" => string, "database" => string, "username" => string, "token" => string}   # only if not back office
-  #   }
-  if cuddy_token
-    status = {"name" => path, "version" => version, "started_at" => start_time, "finished_at" => Time.now, "backoffice" => backoffice}.to_json
-    redis_deploy.set(cuddy_token,status)
+
+  # removes old stuff from the cloud
+  def cleanup
+    # keep the last 4 versions in the cloud
+    if version > 4
+      logger.info("deleting old files from the cloud")
+      storage = Fog::Storage.new(:provider => 'Rackspace', :rackspace_auth_url => config["rackspace_auth_url"], :rackspace_api_key => config["rackspace_api_key"], :rackspace_username => config['rackspace_username'])
+      directory = storage.directories.get("sqshed_apps")
+      img = "#{name}-#{version - 4}.tar.gz"
+      img_file = directory.files.get(img)
+      img_file.destroy if img_file != nil
+    end
+  end
+
+  # pass the ball to the next player (cuddy)
+  def register
+    puts "register"
+    current_path = File.expand_path(File.dirname(__FILE__))
+  	config = YAML.load_file(current_path + "/config.yml")
+  	#  {"version" => integer,      # the version number
+    #   "name" => string,           # the name of the app
+    #   "status" => string,         # starts with "waiting"
+    #   "started_at" => datetime,   # the time when the app was added in the queue
+    #   "finished_at" => datetime,  # the time when the app was properly deployed
+    #     "db" => {"hostname" => string, "database" => string, "username" => string, "token" => string}
+    #   }
+    # }
+    status = {"status" => "queued for deployment", "version" => version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
+    @redis_global.set(build.name, status)
+    # passing the ball to cuddy
+    # key is token of the cuddy node, value is array, each item using following format : 
+    #   {  "name" => string,           # the name of the app
+    #      "version" => integer,       # the version number of the app
+    #      "db_string" => string,      # basis for pwd
+    # }
+    status_hash = {"name" => name, "version" => version, "db_string" => db_string}
+    queue = JSON.parse(@redis_cuddy.get(cuddy_token)) if @redis_cuddy.get(cuddy_token)
+    queue ||= Array.new
+    queue << status_hash
+    @redis_cuddy.set(cuddy_token, queue.to_json)
   end
 end
 
-logger("info", "starting")
-
+logger.info ("Starting the wait cycle")
 while true
   queue = JSON.parse(@redis.get("queue"))
-  logger("info", "QUEUE is #{queue.count} deep") if queue.count > 0
   while queue.count != 0
-    repository = queue.pop
+    app = queue.pop
+    # queue is a array v0.1
+    # each item has following format :
+    #   {  "name" => string,           # the name of the app
+    #      "repository" => string,     # the url of the git repository
+    #       "db_string" => string,     # basis for the pwd
+    #      "cuddy_token" => string     # the token of the host that will host it
+    #   }
+    build = Build.new(app['name'], app['repository'], app['db_string'], app['cuddy_token'])
+    start_time = build.start_time_from_redis
     @redis.set("queue",queue.to_json)
-    status = {"status" => "queued", "version" => repository['version'], "started_at" => Time.now, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
-    @redis.set(repository['repository'], status)
-    logger("info", "starting work on #{repository['repository']}")
-    build(repository['repository'], repository['version'], repository['backoffice'] || false, repository['cuddy_token'])
+    # global status v0.1
+    # the key is the name of the app (unique in the db, a uid could be used)
+    # {  "status" => "queued for build",        # status of the app
+    #    "version" => repository['version'],    # version currently in the pipe
+    #    "started_at" => Time.now,              # start time of the process (push from the front)
+    #    "finished_at" => Time.now,             # finish time of the process (time of the current status)
+    #    "error" => {"message" => "", "backtrace" => ""}
+    #    }
+    # todo remove this line should be done by front
+    status = {"status" => "queued for build", "version" => build.version, "started_at" => start_time, "finished_at" => Time.now, "error" => {"message" => "", "backtrace" => ""}}.to_json
+    @redis_global.set(build.name, status)
+    logger.info("starting work on #{build.name}")
+    fork do
+      build.run
+      build.save
+      build.upload
+      build.register
+      build.cleanup
+    end
   end
+  
   Signal.trap("QUIT") do
-    logger("info", "quitting (received SIGQUIT)")
+    logger.info("quitting (received SIGQUIT)")
     exit
   end
   Signal.trap("KILL") do
-    logger("info", "quitting (received SIGKILL)")
+    logger.info("quitting (received SIGKILL)")
     exit
   end
   Signal.trap("TERM") do
-    logger("info", "quitting (received SIGTERM)")
+    logger.info("quitting (received SIGTERM)")
     exit
   end
 end
